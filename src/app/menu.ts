e2c5bda5ee67883ef152e852c917fbae14b05fff
Giselle Machado <gisellemachadoyes@gmail.com>
2019-07-21 23:08:58 -0400

    routing creado. service mock-up. Observable. Filter de Tipos de Menus

---
+++ src/app/menu.ts
@@ -0,0 +1,12 @@
+export class Menu {
+    public id: string;
+    public type: string;
+    public name: string;
+      public price: string;
+      constructor(id,type, nombre, precio) {
+          this.id = id;
+          this.id = type;
+          this.name = nombre;
+          this.price = precio;
+      }
+  }
